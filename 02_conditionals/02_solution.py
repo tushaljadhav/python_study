033ed65b8dd0ca1dc0b26c417186f68f550d6420
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-12-07 13:08:53 +0000

    create file 02_solution.py

---
+++ 02_conditionals/02_solution.py
@@ -0,0 +1,9 @@
+age = int(input("enter the age :"))
+
+day = "Wednesday"
+
+price = 12 if age >= 18 else 8
+
+if day == "Wednesday":
+    discout = price - 2
+print(f"tiket price for you is ${discout}")
