d558d1ec775c3ace20788fac49518a5470999172
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-12-12 14:07:00 +0000

    09 solution.py

---
+++ 02_conditionals/09_solution.py
@@ -0,0 +1,7 @@
+year = int(input("Enter the Year :"))
+
+if (year % 400 == 0) or (year % 4 == 0 and year % 100 !=0):
+    print(year," is a leap year")
+else:
+    print(year," if NOT a leap year")
+    
