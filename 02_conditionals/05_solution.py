b3ff487cdd5d83ba1da103f9e540e0149e4bc8ef
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-12-12 12:35:03 +0000

    05 solution is completed

---
+++ 02_conditionals/05_solution.py
@@ -0,0 +1,10 @@
+weather = input("Enter A Weather :")
+
+if weather == "Sunny":
+    activity = "Go outside"
+elif weather == "Rainy":
+    activity = "Use umbrella"
+elif weather == "Snowy":
+    activity = "Wear jacket"
+
+print(activity)
