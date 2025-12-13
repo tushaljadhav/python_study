643af08916c539f6f12ecb6e501516fd422ca43a
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-12-13 13:56:54 +0000

    01 solution is completed

---
+++ 03_loops/01_solution.py
@@ -0,0 +1,10 @@
+numbers = [1, -2, 3, -4, 5, 6 ,-9 ,1, 2]
+positive_number_count = 0
+
+for num in numbers :
+    if num > 0:
+        positive_number_count += 1
+print(positive_number_count)
+        
+
+
